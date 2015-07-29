6ae731b250c43682d08ccbc7c2807c7c95f14ed7
Pyohwan <phjang1983@daum.net>
2015-07-29 18:46:26 +0900

    first commit

---
+++ first/src/main/java/FirstGradle.java
@@ -0,0 +1,9 @@
+public class FirstGradle {
+	public FirstGradle() {
+	}
+
+	public static void main(String arg[]) {
+		FirstGradle gradle = new FirstGradle();
+		System.out.println("Hello Gradle with Java");
+	}
+}
